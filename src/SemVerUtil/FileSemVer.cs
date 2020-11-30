f61f4fb3b16eeadbbdc15c9e586dde73745e0928
Scott Herrmann - GitHub Hiddent Email <1109038+daborg69@users.noreply.github.com>
2020-11-30 05:18:03 -0500

    SemVerUtil Started

---
+++ src/SemVerUtil/FileSemVer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace SemVerUtil
+{
+
+	public class FileSemVer
+	{
+		public FileSemVer (string name, string prefix = "") {
+			if (string.IsNullOrEmpty(name)) throw new ArgumentException("PathName cannot be null, must specify the name of a fully qualified path.");
+
+			string semVerPart = "";
+
+			if ( prefix != string.Empty ) {
+				if ( !name.StartsWith(prefix) )
+					throw new ArgumentException(string.Format("PathName [{name}] did not start with the specified prefix [{prefix}]"));
+
+				semVerPart = name.Substring(prefix.Length - 1);
+			}
+			else
+				semVerPart = name;
+
+
+			Name = name;
+			Prefix = prefix;
+			SemVer = semVerPart;
+		}
+
+
+
+		public string Name { get; private set; }
+		public string Prefix { get; private set; }
+		public string SemVer { get; private set; }
+	}
+}
---
+++ src/SemVerUtil/SemVerUtil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SemVerUtil
+{
+	public static class SemVerUtil
+	{
+		public static string DirectoryMostRecentVersion (string path, string prefix) {
+			if (! Directory.Exists(path)) throw new ArgumentException("The directory [{0}] could not be found.", path);
+
+			string searchPattern = prefix + "*";
+			List<string> directories = Directory.GetDirectories(path, searchPattern, SearchOption.TopDirectoryOnly).ToList();
+
+			if ( directories.Count == 0 ) return null;
+
+			
+			
+		}
+	}
+}
